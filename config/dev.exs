6c747216736c688cd5af4d1e1ce0ba27020fe539
Thomas Brewer <tom.brewer@mac.com>
2023-09-13 03:54:04 -0400

    WIP

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :event_relay, ER.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "127.0.0.1",
   database: "event_relay_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
