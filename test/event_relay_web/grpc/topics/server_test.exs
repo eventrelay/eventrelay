a6c96a39a45ce7edfd07c536aa1f06d77f439dea
Thomas Brewer <tom.brewer@mac.com>
2023-11-09 05:53:50 -0500

    [#8] adding initial topics grpc api tests (#11)

---
+++ test/event_relay_web/grpc/topics/server_test.exs
@@ -0,0 +1,50 @@
+defmodule ERWeb.Grpc.EventRelay.Topics.ServerTest do
+  use ER.DataCase
+
+  alias ERWeb.Grpc.EventRelay.Topics.Server
+  alias ER.Events
+
+  alias ERWeb.Grpc.Eventrelay.{
+    CreateTopicRequest,
+    DeleteTopicRequest,
+    ListTopicsRequest
+  }
+
+  describe "create_topic/2" do
+    test "create a new topic" do
+      request = %CreateTopicRequest{
+        name: "audit_log"
+      }
+
+      result = Server.create_topic(request, nil)
+
+      refute ER.Events.get_topic(result.topic.id) == nil
+    end
+  end
+
+  describe "delete_topic/2" do
+    test "deletes a topic" do
+      {:ok, topic} = Events.create_topic(%{name: "log"})
+
+      request = %DeleteTopicRequest{
+        id: topic.id
+      }
+
+      result = Server.delete_topic(request, nil)
+
+      assert ER.Events.get_topic(result.topic.id) == nil
+    end
+  end
+
+  describe "list_topics/2" do
+    test "list topic" do
+      {:ok, _topic} = Events.create_topic(%{name: "log"})
+      {:ok, _topic} = Events.create_topic(%{name: "carts"})
+
+      request = %ListTopicsRequest{}
+
+      result = Server.list_topics(request, nil)
+      assert Enum.count(result.topics) == 2
+    end
+  end
+end
