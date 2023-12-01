17fb9a3a4efe2b81b59a751f3c7f5d65deb9d3d2
Thomas Brewer <tom.brewer@mac.com>
2023-12-01 08:45:55 -0500

    tweaking config for ci

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,7 +34,7 @@ config :hammer,
        redis_url: System.get_env("ER_REDIS_URL")
      ]}
 
-if config_env() == :dev do
+if config_env() == :dev || config_env() == :test do
   database_url =
     System.get_env("ER_DATABASE_URL") ||
       raise """
@@ -42,16 +42,6 @@ if config_env() == :dev do
       For example: ecto://USER:PASS@HOST/DATABASE
       """
 
-  # Configure your database
-  config :event_relay, ER.Repo,
-    url: database_url,
-    stacktrace: true,
-    show_sensitive_data_on_connection_error: true,
-    pool_size: 10
-end
-
-if config_env() == :test do
-  database_url = "postgres://postgres@postgres:5432/event_relay_test"
   # Configure your database
   config :event_relay, ER.Repo,
     url: database_url,
