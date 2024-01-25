cac14a70541705dae1c39ee435313f0052d9717d
Thomas Brewer <tom.brewer@mac.com>
2024-01-24 20:56:19 -0500

    feat(transformers): adding query to transformer

--- lib/event_relay/transformers/transformer.ex
+++ lib/event_relay/transformers/transformer.ex
@@ -2,6 +2,7 @@ defmodule ER.Transformers.Transformer do
   use Ecto.Schema
   import Ecto.Changeset
   alias ER.Sources.Source
+  alias __MODULE__
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -10,14 +11,20 @@ defmodule ER.Transformers.Transformer do
     belongs_to(:source, Source)
     field(:type, Ecto.Enum, values: [:lua, :liquid])
     field(:return_type, Ecto.Enum, values: [:map])
-
+    field :query, :string
     timestamps()
   end
 
   @doc false
   def changeset(transformer, attrs) do
     transformer
-    |> cast(attrs, [:script, :source_id, :return_type])
+    |> cast(attrs, [:script, :source_id, :return_type, :query])
     |> validate_required([:script, :return_type])
+  end
+
+  def matches?(%Transformer{query: nil}, _data), do: true
+
+  def matches?(%Transformer{query: query}, data) do
+    if Flamel.present?(query), do: Predicated.test(query, data), else: true
   end
 end
