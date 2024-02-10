56783ea03361e10ade0273b6a38472201a707cf4
Thomas Brewer <tom.brewer@mac.com>
2024-02-10 06:05:30 -0500

    feat(destinations): adding logic to change retry logic based on HTTP resposne

--- lib/event_relay/destinations/pipeline/webhook/retries.ex
+++ lib/event_relay/destinations/pipeline/webhook/retries.ex
@@ -1,5 +1,5 @@
 defmodule ER.Destinations.Pipeline.Webhook.Retries do
-  def next(destination, _delivery, attempts, now \\ DateTime.utc_now()) do
+  def next(destination, response, attempts, now \\ Flamel.Moment.CurrentTime.utc_now()) do
     # we a substracting 1 because the strategy will increment
     # the attempt count 
     attempt = Enum.count(attempts) - 1
@@ -20,10 +20,47 @@ defmodule ER.Destinations.Pipeline.Webhook.Retries do
     # we need to turn this into minutes
     interval = (strategy.interval * 60) |> Flamel.to_integer()
 
-    if Flamel.Context.halted?(strategy) do
-      {strategy, nil}
-    else
-      {strategy, DateTime.add(now, interval, :millisecond)}
+    available_at =
+      if Flamel.Context.halted?(strategy) do
+        nil
+      else
+        DateTime.add(now, interval, :millisecond)
+      end
+
+    # alter based on http response
+    # TODO: move this into Flamel HTTP strategy
+    case {strategy, available_at, response} do
+      # response is a 410 GONE so halt retries
+      {strategy, _, {:error, %{status: 410}}} ->
+        {Flamel.Context.halt!(strategy, "HTTP 410 GONE"), nil}
+
+      {strategy, original_available_at, {:error, %{headers: %{"RETRY-AFTER" => retry_after}}}} ->
+        available_at = parse_retry_after(retry_after, now, original_available_at)
+        {strategy, available_at}
+
+      # default case
+      {strategy, available_at, _} ->
+        {strategy, available_at}
+    end
+  end
+
+  defp parse_retry_after(nil, _now, original_available_at), do: original_available_at
+  defp parse_retry_after("", _now, original_available_at), do: original_available_at
+
+  defp parse_retry_after(retry_after, now, _original_available_at) when is_integer(retry_after) do
+    retry_after = Flamel.to_integer(retry_after)
+    DateTime.add(now, retry_after, :second)
+  end
+
+  defp parse_retry_after(retry_after, now, original_available_at) do
+    case Calendar.DateTime.Parse.rfc2822_utc(retry_after) do
+      {:ok, datetime} ->
+        datetime
+
+      _ ->
+        retry_after
+        |> Flamel.to_integer()
+        |> parse_retry_after(now, original_available_at)
     end
   end
 end
